b475563b1bc6ec1da95f723d9311a71928b35fd1
Ubuntu <ubuntu@ip-10-0-1-143.ec2.internal>
2026-08-03 11:02:29 +0000

    This is the second ec2 task

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.json({
     status: 'success',
-    message: 'CI/CD  and i have written this to test ci cd is working!. This line is to test the gcm github webhook ',
+    message: 'CI/CD  and i have written this to test ci cd is working!. This line is to test the gcm github webhook. This line is to test the second ec2 dockerized task.......',
     timestamp: new Date()
   });
 });
